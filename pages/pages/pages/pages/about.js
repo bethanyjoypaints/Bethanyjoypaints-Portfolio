0f42f01f491fdf873a513a3143f6ff8fe635eb9d
bethanyjoypaints <bethanyjoypaints@gmail.com>
2025-09-18 21:55:46 -0500

    Create about.js

---
+++ pages/pages/pages/pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main style={{padding:'6rem 1.25rem'}}>
+      <h1 style={{fontSize:'2rem', fontWeight:700}}>About</h1>
+      <p>Short bio goes here.</p>
+    </main>
+  )
+}

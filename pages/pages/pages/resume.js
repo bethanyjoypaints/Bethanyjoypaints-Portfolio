619fbad14f8d673cdf1efd9e80baccd0bbcdbd3a
bethanyjoypaints <bethanyjoypaints@gmail.com>
2025-09-18 21:55:26 -0500

    Create resume.js

---
+++ pages/pages/pages/resume.js
@@ -0,0 +1,8 @@
+export default function Resume() {
+  return (
+    <main style={{padding:'6rem 1.25rem'}}>
+      <h1 style={{fontSize:'2rem', fontWeight:700}}>Resume</h1>
+      <p>Upload your PDF later to /resume/Resume.pdf.</p>
+    </main>
+  )
+}

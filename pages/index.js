e88b3b85c00a8bef264f373ca7131f622389eb52
bethanyjoypaints <bethanyjoypaints@gmail.com>
2025-09-18 22:24:24 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{padding:'6rem 1.25rem'}}>
       <h1 style={{fontSize:'2rem', fontWeight:700}}>Bethany Joy Paints</h1>
-      <p>Home page is live 🎉</p>
+      <p>Welcome! The homepage is working 🎉</p>
     </main>
   )
 }
